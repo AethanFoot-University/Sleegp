200031f6a8c2efc38ded69a5ea94fc74ce9089b7
Søren Mortensen <soren@sorenmortensen.com>
2019-02-12 15:27:09 +0000

    Add app/
    Signed-off-by: Søren Mortensen <soren@sorenmortensen.com>

---
+++ app/src/EEGApp.java
@@ -0,0 +1,13 @@
+//
+//  EEGApp
+//  EEG App
+//
+//  Created by Søren Mortensen on 2019-02-12.
+//  Copyright © 2019 CSED Group 11. All rights reserved.
+//
+
+public class EEGApp {
+    public static void main(String[] args) {
+        System.out.println("Hello from the EEG App!");
+    }
+}
