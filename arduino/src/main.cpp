d55940ea1c0feaeab2f728b5222f8aa5f40914af
Søren Mortensen <soren@sorenmortensen.com>
2019-02-12 15:46:59 +0000

    Add some source code and correct config for Arduino
    Signed-off-by: Søren Mortensen <soren@sorenmortensen.com>

---
+++ arduino/src/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  arduino
+//
+//  Created by Søren Mortensen on 2019-02-12.
+//  Copyright © 2019 CSED Group 11. All rights reserved.
+//
+
+#include <Arduino.h>
+
+void setup() {
+    
+}
+
+void loop() {
+    
+}
