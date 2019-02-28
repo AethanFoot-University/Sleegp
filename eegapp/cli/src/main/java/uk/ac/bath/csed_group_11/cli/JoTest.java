fa08c30900a87ccef49161de644cc17ee7706bea
Mathew Allington <funkymallington@gmail.com>
2019-02-28 15:14:44 +0000

    Base

---
+++ eegapp/cli/src/main/java/uk/ac/bath/csed_group_11/cli/JoTest.java
@@ -0,0 +1,26 @@
+package uk.ac.bath.csed_group_11.cli;
+
+import java.util.Scanner;
+
+public class JoTest {
+
+    public static Scanner in;
+    public static void main(String[] main){
+        in = new Scanner(System.in);
+    }
+
+    public void testHeadsetConnection(){
+
+    }
+
+    public void testDataComingBackFromHeadset(){
+
+    }
+
+    public void testPlayBackFunction(){
+
+    }
+    public void testBlinkDetection(){
+
+    }
+}
