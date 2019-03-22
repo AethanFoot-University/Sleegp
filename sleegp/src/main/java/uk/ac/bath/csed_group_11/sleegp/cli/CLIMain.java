8470eab5afa6c25ddf0ecd9ac0469ced42151de5
Søren Mortensen <soren@neros.dev>
2019-03-22 15:17:39 +0000

    Implement command-line parsing using Docopt
    Signed-off-by: Søren Mortensen <soren@neros.dev>

--- sleegp/src/main/java/uk/ac/bath/csed_group_11/sleegp/cli/CLIMain.java
+++ sleegp/src/main/java/uk/ac/bath/csed_group_11/sleegp/cli/CLIMain.java
@@ -8,23 +8,41 @@ package uk.ac.bath.csed_group_11.sleegp.cli;
 //  Copyright © 2019 CSED Group 11 Developers. All rights reserved.
 //
 
-import uk.ac.bath.csed_group_11.sleegp.cli.args.Arguments;
-import uk.ac.bath.csed_group_11.sleegp.cli.args.ArgumentsException;
-import uk.ac.bath.csed_group_11.sleegp.cli.args.Options;
+import org.docopt.Docopt;
 import uk.ac.bath.csed_group_11.sleegp.logic.data.Epoch;
 import uk.ac.bath.csed_group_11.sleegp.logic.data.EpochContainer;
 import uk.ac.bath.csed_group_11.sleegp.logic.hardware.Headset;
 import uk.ac.bath.csed_group_11.sleegp.logic.hardware.SimulatedHeadset;
 import uk.ac.bath.csed_group_11.sleegp.logic.util.ObjectConverter;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.NoSuchFileException;
+import java.io.*;
+import java.util.Map;
 
 /**
  * Contains the main entry point for command-line execution of slEEGp.
  */
 public class CLIMain {
+    /**
+     * Usage message for the CLI; also defines the format for acceptable arguments using Docopt.
+     *
+     * @see org.docopt.Docopt
+     * @since 0.3.0
+     */
+    public static final String doc =
+        "Usage:\n" +
+        "  sleegp record [-o <output-file>]\n" +
+//        "  sleegp simulate <data-file>\n" +
+        "  sleegp convert [-o <output-file>] <input-file>\n" +
+        "  sleegp -h | --help | --version\n" +
+        "\n" +
+        "Arguments:\n" +
+//        "  <data-file>   recorded data file to simulate from\n" +
+        "  <input-file>  file to convert, in .ec or .csv format\n" +
+        "\n" +
+        "Options:\n" +
+        "  -o, --output <output-file>  output file location\n" +
+        "  -h, --help                  show this help message and exit\n" +
+        "  --version                   show version and exit\n";
 
     /**
      * Main command-line entry point for slEEGp.
@@ -32,65 +50,52 @@ public class CLIMain {
      * @param args The command line arguments.
      */
     public static void main(String[] args) {
-        // Parse options from the provided command line arguments.
-        Options options = Options.defaultOptions();
-        try {
-            options = new Arguments(args).parse();
-        } catch (ArgumentsException e) {
-            System.err.println(e.toString());
-            System.exit(1);
-        }
+        Map<String, Object> opts = new Docopt(doc).withVersion("sleegp 0.3.0").parse(args);
+        System.out.println(opts);
 
-        if (options.shouldSimulate()) {
-            var maybeSimFile = options.getSimulationFile();
-
-            if (maybeSimFile.isEmpty()) {
-                System.err.println("Unexpected error: value not found for simulation file path.");
-                System.exit(2);
-            }
-
-            try {
-                CLIMain.simulateHeadset(maybeSimFile.get());
-            } catch (NoSuchFileException e) {
-                System.err.println("Unable to simulate headset from file \"" + maybeSimFile.get() +
-                        "\": File does not exist.");
-            } catch (IOException e) {
-                System.err.println("Unable to simulate headset from file \"" + maybeSimFile.get() +
-                        "\": " + e.toString());
-                System.exit(1);
-            } catch (ClassNotFoundException e) {
-                System.err.println("File \"" + maybeSimFile.get() + "\" tries to load class that " +
-                        "was not found: " + e.toString());
-                System.exit(1);
-            }
-        } else {
+        if (opts.get("record").equals(true)) {
             EpochContainer ec = new EpochContainer();
 
 //            ec.setAutoSave(auto, 10000);
+
+            final PrintWriter out;
+
+            var maybeOutputFile = (String)opts.get("--output");
+            if (maybeOutputFile != null) {
+                try {
+                    out = new PrintWriter(new FileOutputStream(maybeOutputFile), true);
+                } catch (FileNotFoundException e) {
+                    System.err.println("Unable to write to specified output file: " +
+                            e.getMessage());
+                    return;
+                }
+            } else {
+                out = new PrintWriter(System.out, true);
+            }
 
             int epochLost = 0;
             Headset head = new Headset() {
                 @Override
                 public void update(Epoch data) {
                     if (data.getPoorSignalLevel() < 100) {
-                        System.out.println("Recording [Poor Signal Level: "
+                        out.println("Recording [Poor Signal Level: "
                                 + data.getPoorSignalLevel() + "]");
                         ec.addEpoch(data);
-                        System.out.println(data);
+                        out.println(data);
                     } else {
-                        System.out.println("Headset not on at: " + data.getTimeStamp());
+                        out.println("Headset not on at: " + data.getTimeStamp());
                     }
                 }
             };
 
-            head.addBlinkListener(() -> System.out.println("Stop blinking"));
-            head.addRemovedHeadsetListener(() -> System.out.println("Headset removed"));
-            head.addPutOnHeadsetListener(() -> System.out.println("Headset put on"));
+            head.addBlinkListener(() -> out.println("Stop blinking"));
+            head.addRemovedHeadsetListener(() -> out.println("Headset removed"));
+            head.addPutOnHeadsetListener(() -> out.println("Headset put on"));
 
             if (head.capture()) {
-                System.out.println("Connected");
+                out.println("Connected");
             } else {
-                System.out.println("Connection failed");
+                out.println("Connection failed");
             }
 
             try {
@@ -102,6 +107,56 @@ public class CLIMain {
             } catch (InterruptedException e) {
                 System.err.println("Thread interrupted: " + e.toString());
                 Thread.currentThread().interrupt();
+            }
+//        } else if (opts.get("simulate").equals(true)) {
+//            var fileName = (String)opts.get("<data-file>");
+//
+//            try {
+//                CLIMain.simulateHeadset(fileName);
+//            } catch (NoSuchFileException e) {
+//                System.err.println("Unable to simulate headset from file \"" + fileName +
+//                        "\": File does not exist.");
+//                System.exit(1);
+//            } catch (IOException e) {
+//                System.err.println("Unable to simulate headset from file \"" + fileName +
+//                        "\": " + e.toString());
+//                System.exit(1);
+//            } catch (ClassNotFoundException e) {
+//                System.err.println("File \"" + fileName + "\" tries to load class that was not " +
+//                        "found: " + e.toString());
+//                System.exit(1);
+//            }
+        } else if (opts.get("convert").equals(true)) {
+            var ecPath = (String)opts.get("<input-file>");
+            EpochContainer ec;
+
+            try {
+                ec = EpochContainer.loadContainerFromFile(new File(ecPath));
+            } catch (IOException | ClassNotFoundException e) {
+                System.err.println("Unable to load container from file: " + e.toString());
+                return;
+            }
+
+            String converted = ec.genCSV();
+
+            var maybeOutputFile = (String)opts.get("--output");
+            if (maybeOutputFile != null) {
+                var file = new File(maybeOutputFile);
+
+                try (var fop = new FileOutputStream(file)) {
+                    if (!file.exists()) file.createNewFile();
+
+                    byte[] content = converted.getBytes();
+
+                    fop.write(content);
+                    fop.flush();
+                } catch (FileNotFoundException e) {
+                    System.err.println("Output file not found: " + e.toString());
+                } catch (IOException e) {
+                    System.err.println("Could not write converted data to file: " + e.toString());
+                }
+            } else {
+                System.out.println(converted);
             }
         }
     }
