538652f29c700e468c8e7e5a3a0f0fc9ca61db27
Mathew Allington <funkymallington@gmail.com>
2019-03-28 17:55:38 +0000

    New Quick Sort Algorithm for Comparable objects

---
+++ sleegp/src/main/java/uk/ac/bath/csed_group_11/sleegp/logic/data/QuickSort.java
@@ -0,0 +1,67 @@
+package uk.ac.bath.csed_group_11.sleegp.logic.data;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class QuickSort <t extends Comparable<t>>{
+    private List<t> toSort;
+
+    public QuickSort(List<t> toSort){
+        this.toSort = toSort;
+    }
+
+    public List<t> quickSort(){
+        quickSort(toSort, 0, toSort.size()-1);
+        return toSort;
+    }
+
+    private void quickSort(List<t> nums, int start, int pivot) {
+
+        int partition = partition(nums, start, pivot);
+
+        if (partition - 1 > start) {
+            quickSort(nums, start, partition - 1);
+        }
+        if (partition + 1 < pivot) {
+            quickSort(nums, partition + 1, pivot);
+        }
+    }
+
+    private int partition(List<t> nums, int start, int pivot) {
+        t pivotNum = nums.get(pivot);
+
+        for (int i = start; i < pivot; i++) {
+
+            if (nums.get(i).compareTo(pivotNum) <0) {
+                t temp = nums.get(start);
+                nums.set(start, nums.get(i));
+                nums.set(i, temp);
+                start++;
+            }
+        }
+
+        t temp = nums.get(start);
+        nums.set(start, pivotNum);
+        nums.set(pivot, temp);
+
+        return start;
+    }
+
+
+    /**
+     * Example implementation of a quick sort
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        //Example implementation
+        //Note that object has to implement comparable
+        List<Date> list = Arrays.asList(new Date(2323223232L), new Date(323223232L) ,new Date(232323232L), new Date(2357823232L));
+        System.out.println(list.toString());
+        QuickSort<Date> sorter = new QuickSort<>(list);
+
+        System.out.println(sorter.quickSort().toString());
+
+    }
+}
