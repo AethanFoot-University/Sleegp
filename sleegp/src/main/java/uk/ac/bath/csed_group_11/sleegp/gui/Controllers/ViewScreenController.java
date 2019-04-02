a38ac5520a8fbbc0311652e51a5017dee7428ee0
Mathew Allington <funkymallington@gmail.com>
2019-04-02 22:40:18 +0100

    Trying graphs

--- sleegp/src/main/java/uk/ac/bath/csed_group_11/sleegp/gui/Controllers/ViewScreenController.java
+++ sleegp/src/main/java/uk/ac/bath/csed_group_11/sleegp/gui/Controllers/ViewScreenController.java
@@ -1,6 +1,12 @@
 package uk.ac.bath.csed_group_11.sleegp.gui.Controllers;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -8,7 +14,18 @@ import java.util.ResourceBundle;
 /**
  * To be written by: Mathew
  */
+
+
 public class ViewScreenController implements Initializable {
+    @FXML
+    LineChart mainChart;
+
+    @FXML
+    CategoryAxis xAxis;
+
+    @FXML
+    NumberAxis yAxis;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
